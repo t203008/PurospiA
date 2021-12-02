150a183d59bfe8dbab6525b89b4143c35d873500
Ryosuke Ohtsuki <91816208+t203008@users.noreply.github.com>
2021-12-03 02:32:43 +0900

    main

--- code.py
+++ code.py
@@ -2,4 +2,5 @@ import streamlit as st
 import pandas as pd
 batter=st.file_uploader("野手", type='csv')
 pitcher=st.file_uploader("投手", type='csv')
-st.write(pd.read_csv(batter))
+batter_df=pd.read_csv(batter,index_col=0)
+st.write(batter_df)
