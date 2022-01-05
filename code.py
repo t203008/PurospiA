3dfe7ba96f1ba34173668a5b7cc881736e547112
t203008 <91816208+t203008@users.noreply.github.com>
2022-01-05 09:53:44 +0900

    Update code.py

--- code.py
+++ code.py
@@ -3,10 +3,12 @@ import pandas as pd
 st.title("称号")
 position=st.radio("",("投手","野手"))
 go=st.radio("称号はどう決めますか。",("","自分で","おまかせ"))
-if go=="自分で":
-  
-if go=="おまかせ":
-  target=st.multiselect("称号の目的は何ですか。",["同値","能力をAに","弱点克服","得意強化"],)
+if position=="投手":
+  if go=="自分で":
+    target=st.multiselect("上げたい能力は何ですか",["球威","制球","スタミナ","スピリッツ"])
+    if target=="球威" or "制球" or "スタミナ"
+  if go=="おまかせ":
+    target=st.multiselect("称号の目的は何ですか。",["同値","能力をAに","弱点克服","得意強化","スピリッツ補強"],)
 start=st.button("実行")
 if start==True:
   st.write("よし")
