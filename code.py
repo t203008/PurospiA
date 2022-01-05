300530a7f59f3f700bc073df188d1b588720d525
t203008 <91816208+t203008@users.noreply.github.com>
2022-01-05 13:36:03 +0900

    Update code.py

--- code.py
+++ code.py
@@ -12,11 +12,11 @@ if go=="自分で":
     if target1=="球威" or "制球" or "スタミナ":
       tarL2.remove(target1)
       sta1=st.slider("この能力を最低どのくらい上げたいですか。",min_value=0,max_value=3)
+      target2=st.selectbox("上げたい能力②は何ですか",tarL2)
+      sta2=st.slider("この能力をどの程度上げたいですか。",min_value=0,max_value=3)
     else:
       star=st.slider("この能力を最低どのくらい上げたいですか。",min_value=0,max_value=30)
-      
-    target2=st.selectbox("上げたい能力②は何ですか",tarL2)
-    sta2=st.slider("この能力をどの程度上げたいですか。",min_value=0,max_value=3)
+
   if go=="おまかせ":
     target=st.multiselect("称号の目的は何ですか。",["同値","能力をAに","弱点克服","得意強化","スピリッツ補強"],)
 start=st.button("実行")
