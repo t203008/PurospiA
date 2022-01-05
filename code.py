5c59eaad75c8eaea4e9ace3d5dc23630e4c2ad0a
t203008 <91816208+t203008@users.noreply.github.com>
2022-01-05 09:59:38 +0900

    Update code.py

--- code.py
+++ code.py
@@ -5,9 +5,11 @@ position=st.radio("",("投手","野手"))
 go=st.radio("称号はどう決めますか。",("ランダム","自分で","おまかせ"))
 if go=="自分で":
   if position=="投手":
-    target=st.selectbox("上げたい能力①は何ですか",["球威","制球","スタミナ","スピリッツ"])
-    if target=="球威" or "制球" or "スタミナ":
+    target1=st.selectbox("上げたい能力①は何ですか",["球威","制球","スタミナ","スピリッツ"])
+    if target1=="球威" or "制球" or "スタミナ":
       sta1=st.slider("この能力をどの程度上げたいですか。",min_value=0,max_value=3)
+    target2=st.selectbox("上げたい能力②は何ですか",["球威","制球","スタミナ"])
+    sta=st.slider("この能力をどの程度上げたいですか。",min_value=0,max_value=3)
   if go=="おまかせ":
     target=st.multiselect("称号の目的は何ですか。",["同値","能力をAに","弱点克服","得意強化","スピリッツ補強"],)
 start=st.button("実行")
