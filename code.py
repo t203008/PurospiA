fad525f632f5acc7c73aad3070c9b50fe0c1b56f
t203008 <91816208+t203008@users.noreply.github.com>
2022-01-05 10:00:13 +0900

    Update code.py

--- code.py
+++ code.py
@@ -9,7 +9,7 @@ if go=="自分で":
     if target1=="球威" or "制球" or "スタミナ":
       sta1=st.slider("この能力をどの程度上げたいですか。",min_value=0,max_value=3)
     target2=st.selectbox("上げたい能力②は何ですか",["球威","制球","スタミナ"])
-    sta=st.slider("この能力をどの程度上げたいですか。",min_value=0,max_value=3)
+    sta2=st.slider("この能力をどの程度上げたいですか。",min_value=0,max_value=3)
   if go=="おまかせ":
     target=st.multiselect("称号の目的は何ですか。",["同値","能力をAに","弱点克服","得意強化","スピリッツ補強"],)
 start=st.button("実行")
