a57c9269e47d8bee46c772b8b5d8e16a6e1b6325
t203008 <91816208+t203008@users.noreply.github.com>
2022-01-05 10:08:14 +0900

    Update code.py

--- code.py
+++ code.py
@@ -13,7 +13,6 @@ if go=="自分で":
       sta1=st.slider("この能力をどの程度上げたいですか。",min_value=0,max_value=3)
     target2=st.selectbox("上げたい能力②は何ですか",tarL2)
     sta2=st.slider("この能力をどの程度上げたいですか。",min_value=0,max_value=3)
-    
   if go=="おまかせ":
     target=st.multiselect("称号の目的は何ですか。",["同値","能力をAに","弱点克服","得意強化","スピリッツ補強"],)
 start=st.button("実行")
