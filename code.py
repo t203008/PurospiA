c3074214804e85dc59677d6782c6e18cdc1e283b
t203008 <91816208+t203008@users.noreply.github.com>
2022-01-05 09:47:57 +0900

    Update code.py

--- code.py
+++ code.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 st.title("称号")
 position=st.radio("",("投手","野手"))
-go=st.radio("称号はどう決めますか。",("自分で","おまかせ"))
+go=st.radio("称号はどう決めますか。",(,"自分で","おまかせ"))
+if go=="自分で":
+  
 if go=="おまかせ":
   target=st.multiselect("称号の目的は何ですか。",["同値","能力をAに","弱点克服","得意強化"],)
 start=st.button("実行")
