c52ffae69feab780f504723b19a426a349d1059d
t203008 <91816208+t203008@users.noreply.github.com>
2022-01-06 16:44:52 +0900

    Update code.py

--- code.py
+++ code.py
@@ -63,7 +63,10 @@ if start==True:
       star=shogo[(shogo[target1]>=sta1)&(shogo[target2]>=sta2)]
     st.write("称号一覧")
     st.write(star)
-    st.write(star[["星"]].count())
+    st.write(star[star["星"]==1].count())
+    st.write(star[star["星"]==2].count())
+    st.write(star[star["星"]==3].count())
+    st.write(star[star["星"]==4].count())
     
     
     per=pd.DataFrame(data={"星":["☆1","☆2","☆3","☆4"]})
