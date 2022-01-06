fb29a77e270eae4878554e9f87cb3ef7bc862f20
t203008 <91816208+t203008@users.noreply.github.com>
2022-01-06 11:37:26 +0900

    Update code.py

--- code.py
+++ code.py
@@ -63,5 +63,5 @@ if start==True:
       star=shogo[(shogo[target1]>=sta1)&(shogo[target2]>=sta2)]
     st.write("称号一覧")
     st.write(star)
-    per = star.pivot_table(index="星")
+    per = star.pivot_table(index="星",aggfunc="count")
     st.write(per)
